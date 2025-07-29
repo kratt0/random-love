4984c8cb3f825106c02473c10df0cc9ccf14b557
kratt0 <vitegonzalo64@gmail.com>
2025-07-29 02:14:37 -0600

    Initial commit: Added love card script
    un tipo proyecto para algunos principiantes (como yo) xd

---
+++ loverandom.py
@@ -0,0 +1,20 @@
+import random
+
+name_pareja = "name"
+
+#aqui estaras poniendo los mensajes que quieres que aparezcan 
+para_ti_random = [
+    "mensaje bonito ",
+    "mensaje bonito",
+    "mensaje bonito",
+    
+]
+random_mensaje = random.choice(para_ti_random)
+#puedes personalizar 
+print(".⋆♱⫘⫘⫘⛧°.⋆༺♱༻⋆.°⛧⫘⫘⫘♱⋆.") #aqui los marcos 
+print("٠࣪⭑   ٠࣪⭑")
+print(f"𓆩♡𓆪 ¡Para {name_nv.upper()}!                           *")
+print("٠࣪⭑    ٠࣪⭑")
+print(f"𓆩♡𓆪 {random_mensaje.ljust(34)}   *") #en el ljust depende de las letras que escribas puedes poner un numero ajustandolo 
+print("٠࣪⭑   ٠࣪⭑")
+print(".⋆♱⫘⫘⫘⛧°.⋆༺♱༻⋆.°⛧⫘⫘⫘♱⋆.")
